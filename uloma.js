b5eb5e406d92b06337375c59d3f849a42ce390b4
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-06-02 16:18:10 +0100

    .

--- uloma.js
+++ uloma.js
@@ -2,9 +2,16 @@
     let concatenate = "Chuka" + " " + "Uloma";
     return concatenate;
 }
-console.log(combine())*/
+console.log(combine())
 function AddNumbers(a,b) {
     let result = a + b;
     return result;
 }
-console.log(AddNumbers(1,2))
+console.log(AddNumbers(1,2))*/
+function AddTwoValues() {
+    let a = 1
+    let b = 2
+    let result = a + b
+    return result;
+} 
+console.log(AddTwoValues())
